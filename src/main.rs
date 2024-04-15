05a1f0a710a88bbb8e83801471c9dd3cef840b17
Seth Derrick <seth@sethderrick.tech>
2024-04-15 17:45:01 -0500

    finished initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use actix_cors::Cors;
-use actix_web::{http::header, web, App, HttpServer, Responder, HttpResponder};
-use serde::{Deserialization, Serialize};
-use reqwest::Client as HttpClient;
-use async_trait::async_trait;
+use actix_web::{http::header, web, App, HttpServer, Responder, HttpResponse};
+use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
 use std::collections::HashMap;
 use std::fs;
-use std::io.Write;
+use std::io::Write;
 
 
 fn main() {
