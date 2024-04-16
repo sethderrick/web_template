25d3b59c08add6c140664cf96ae00ce676c40d36
Seth Derrick <seth@sethderrick.tech>
2024-04-16 12:36:22 -0500

    Finish User CRUD

--- src/main.rs
+++ src/main.rs
@@ -118,6 +118,23 @@ async fn delete_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl
   HttpResponse::Ok().finish()
 }
 
+async fn register_user(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
+  let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
+  db.insert_user(user.into_inner());
+  let _ = db.save_to_file();
+  HttpResponse::Ok().finish()
+}
+
+async fn login(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
+  let db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
+  match db.get_user_by_name(&user.username) {
+    Some(stored_user) if stored_user.password == user.password => {
+      HttpResponse::Ok().body("Logged in!")
+    },
+    _ => HttpResponse::BadRequest().body("Invalid username or password")
+  }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   let db: Database = match Database::load_from_file() {
@@ -148,6 +165,8 @@ async fn main() -> std::io::Result<()> {
           .route("/task", web::put().to(update_task))
           .route("/task/{id}", web::get().to(read_task))
           .route("/task/{id}", web::delete().to(delete_task))
+          .route("/register", web::post().to(register_user))
+          .route("/login", web::post().to(login))
   })
   .bind("127.0.0.1:8080")?
   .run()
